f05e110ed92e39b9a02a33af69a54a49d0a82606
Geoffrey Huntley <ghuntley@ghuntley.com>
2014-02-13 21:23:54 +1100

    added whispirgateway constructor test

---
+++ src/SendSMS.Common.Tests/WhispirGatewayTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using NUnit.Framework;
+using SendSMS.Common.MessageGateways;
+
+namespace SendSMS.Common.Tests
+{
+    [TestFixture]
+    public class WhispirGatewayTests
+    {
+        [Test]
+        public void ConstructorShouldPopulateProperties()
+        {
+            const string auth = "helloworld";
+            const string url = "http://localhost";
+            const string key = "1234";
+
+            var gateway = new WhispirGateway(auth, url, key);
+
+            gateway.ApiAuthorization.Should().Be(auth);
+            gateway.ApiBaseUrl.Should().Be(url);
+            gateway.ApiKey.Should().Be(key);
+        }
+    }
+}

633cf9b5978c9ed8b9a94009de46bde2e4b0f244
Geoffrey Huntley <ghuntley@ghuntley.com>
2014-02-13 20:57:13 +1100

    added test to ensure subject always is a single whitespace

---
+++ src/SendSMS.Common.Tests/WhispirSMSTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using NUnit.Framework;
+using SendSMS.Common.Entities;
+
+namespace SendSMS.Common.Tests
+{
+    [TestFixture]
+    public class WhispirSMSTests : TestBase
+    {
+        [Test]
+        public void ShouldHaveWhitespaceForSubjectToWorkaroundWhispirApiImplementation()
+        {
+            var whispirsms = new WhispirSMS();
+
+            whispirsms.subject.Should().Be(" ");
+        }
+    }
+}
